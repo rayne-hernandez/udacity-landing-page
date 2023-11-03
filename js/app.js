2f842e5fdf4c96543dbb9ef5f3466eb02343b3d6
Rayne Hernandez <rayne.hernandez@protonmail.com>
2023-11-02 21:25:30 -0700

    Adding sections and a responsive navbar with a hamburger menu icon

--- js/app.js
+++ js/app.js
@@ -31,9 +31,33 @@ const navbarList = document.querySelector("#navbar__list");
  * Start Helper Functions
  * 
 */
+function createHamburgerIcon() {
+    // create list element container
+    const li = document.createElement("li");
+    li.classList.add("hamburger");
+
+    // create hamburger bars
+    const span1 = document.createElement("span");
+    span1.classList.add("bar", "first");
+    const span2 = document.createElement("span");
+    span2.classList.add("bar");
+    const span3 = document.createElement("span");
+    span3.classList.add("bar");
+
+    li.appendChild(span1);
+    li.appendChild(span2);
+    li.appendChild(span3);
+    return li;
+}
+
 function populateNavbar() {
     const liFrag = document.createDocumentFragment();
 
+    // create hamburger icon
+    const li = createHamburgerIcon();
+    liFrag.appendChild(li);
+
+    // Create NavLink for each Section
     for (const section of sections) {
         const dataNav = section.getAttribute("data-nav");
         const li = document.createElement("li");
